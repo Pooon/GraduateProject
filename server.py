3226afb994fe2f9bdf2584c542538c69c729e8e4
LiuYihan <641872038@qq.com>
2015-12-04 21:17:06 +0800

    修改符号

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('hello.html', name = ‘here!’)
+    return render_template('hello.html', name = 'ddd')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
