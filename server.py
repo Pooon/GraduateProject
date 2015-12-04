573b83f9f6f0dc5fca0b55f82b3e57b4f0349da7
LiuYihan <641872038@qq.com>
2015-12-04 21:16:07 +0800

    提交模板html

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Mr. Du</h1>'
+    return render_template('hello.html', name = ‘here!’)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
