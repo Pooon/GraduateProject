402f492d61e6ac9ed34ac516f7d831e7da452ad4
LiuYihan <641872038@qq.com>
2015-12-04 22:57:58 +0800

    整理下配置

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,8 @@
 from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('hello.html', name = '6666666')
--- server.py
+++ server.py
@@ -1,18 +1,12 @@
 from gevent import monkey
 monkey.patch_all()
 
-from flask import Flask
-from flask import render_template
+from api import app
 from gevent.wsgi import WSGIServer
 from config import SERVER, PORT
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('hello.html', name = 'ddd')
 
 if __name__ == '__main__':
     app.debug = True
     http_server = WSGIServer((SERVER, PORT), app)
     http_server.serve_forever()
+
