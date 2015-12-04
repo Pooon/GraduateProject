81e69227f15e5d05d676adb14aa05c21ffa3bab5
LiuYihan <641872038@qq.com>
2015-12-05 00:01:36 +0800

    先写死配置

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ monkey.patch_all()
 
 from GraduateProject import app
 from gevent.wsgi import WSGIServer
-from GraduateProject.config import SERVER, PORT
+# from GraduateProject.config import SERVER, PORT
 
 if __name__ == '__main__':
     app.debug = True
-    http_server = WSGIServer((SERVER, PORT), app)
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
 
