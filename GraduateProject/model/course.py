3dd6e1b6d8cf2ceb07ba17cae3b09f789fc7a56c
LiuYihan <641872038@qq.com>
2015-12-05 23:42:53 +0800

    增加course的model

---
+++ GraduateProject/model/course.py
@@ -0,0 +1,14 @@
+
+class course(object):
+
+	TABLE_NAME = 'course'
+
+    class Field(object):
+        _id = '_id'
+        userId = 'userId'
+        title = 'title'
+        categories = 'categories'
+        topics = 'topics'
+        digest = 'digest'
+        price = 'price'
+		
