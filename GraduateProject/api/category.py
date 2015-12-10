c5752803735cd749908a7e88f18202d7ef67dd9b
LiuYihan <641872038@qq.com>
2015-12-10 16:29:27 +0800

    修改编译不通过的问题

--- GraduateProject/api/category.py
+++ GraduateProject/api/category.py
@@ -2,12 +2,16 @@ from GraduateProject import app
 from GraduateProject.model import MySQLConnectionWrap
 from GraduateProject.model.category import Category
 
+class Error(object):
+    CATEGORY_ERROR = {'err': 1001,
+                      'msg': 'Categories is illegal'}
+
 @app.route('/category/detail',methods=['GET'])
 def getCategoryDetail():
 	
 	category_id = request.args['categoryId']
 
 	if category_id is None:
-		return jsonify(stat = 0, ''), 400
+		return jsonify(stat = 0, **Error.CATEGORY_ERROR), 400
 
 	return 'category not null'
