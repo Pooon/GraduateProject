1f9b06c72f64abfcd34cacd7d52a3bdff824e2bb
LiuYihan <641872038@qq.com>
2016-01-08 13:44:04 +0800

    修改数据脚本

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from gevent import monkey
 monkey.patch_all()
 
@@ -19,6 +20,13 @@ def createall():
 
     db.create_all()
 
+@manager.command
+def updateCourse():
+	
+	course = Course.query.get(1)
+	course.update({course_name:'C语言程序设计'})
+	db.session.commit()
+
 def make_shell_context():
     return dict(app=app, db=db)
 
@@ -26,7 +34,7 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     app.debug = True
-    # manager.run() #!use this only in case that python shell environ' is needed
+    manager.run() #!use this only in case that python shell environ' is needed
     http_server = WSGIServer((SERVER, PORT), app)
     http_server.serve_forever()
     
