d5a48a0603c18c440ea0ce835f2e9a84fd356a57
liuyihan <641872038@qq.com>
2016-01-08 20:19:47 +0800

    修改配置项

--- GraduateProject/config/config.py
+++ GraduateProject/config/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess-what-it-is'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:111111@localhost/project'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:111111@localhost/project?charset=utf8'
     
 
     @staticmethod
